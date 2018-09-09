a9acbe5f2e75fba7cd729d45370272e4a167d6ea
Patrick Amaral <patrick.dev.atom@gmail.com>
2018-09-08 22:23:11 -0300

    Finalizado template de detalhes.

--- website/case/models.py
+++ website/case/models.py
@@ -4,9 +4,9 @@ from django.urls import reverse
 class Contato(models.Model):
     nome = models.CharField(max_length = 120, verbose_name='Nome')
     celular = models.IntegerField(verbose_name='Celular')
-    cargo = models.CharField(max_length = 120, verbose_name='Cargo')
-    empresa = models.CharField(max_length = 50, verbose_name='Empresa')
-    email = models.EmailField(max_length = 120, verbose_name='Email')
+    cargo = models.CharField(max_length = 120, verbose_name='Cargo', blank='True')
+    empresa = models.CharField(max_length = 50, verbose_name='Empresa', blank='True')
+    email = models.EmailField(max_length = 120, verbose_name='Email', blank='True')
 
     def get_absolute_url(self):
         return reverse("contato:contato-detail", kwargs={"id": self.id})
