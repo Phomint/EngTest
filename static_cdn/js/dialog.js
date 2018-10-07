66ef8eca432f4440fea9897d07ca60650081cb89
Patrick Amaral <patrick.dev.atom@gmail.com>
2018-10-07 13:16:58 -0300

    Alterações de serviços.

--- static_cdn/js/dialog.js
+++ static_cdn/js/dialog.js
@@ -1,10 +1,15 @@
 var dialog = document.querySelector('dialog');
 var showDialogButton = document.querySelector('#show-dialog');
+var deleteButton = document.querySelector('#demo-show-snackbar');
+
 if (! dialog.showModal) {
     dialogPolyfill.registerDialog(dialog);
 }
 showDialogButton.addEventListener('click', function() {
     dialog.showModal();
+});
+deleteButton.addEventListener('click',function(){
+  dialog.close();
 });
 dialog.querySelector('.close').addEventListener('click', function() {
     dialog.close();
